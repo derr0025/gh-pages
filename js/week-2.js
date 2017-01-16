f22474e7b59e8025f46d58b64469e0b80aa27b2a
Brianna Derro <derr0025@algonquinlive.com>
2017-01-16 13:16:08 -0500

    Update week-2.js

--- js/week-2.js
+++ js/week-2.js
@@ -1,1 +1,94 @@
 
+//Task 1- head-start
+/*Declare three variables, a and b and c.
+
+If a has value 3, b has value 5, and c doesn't have a value, alert the following expressions ( one after another ). Alert popup box has to show the following content for each expression (concatenate everything in one long expression using "\n" to make new lines): */
+
+/*
+var a = 3;
+var b = 5;
+var c;
+
+alert("var a = 3;\nvar b = 5;\nvar c;\n--------\n\n" + 
+     "a + b = " + (a + b) + "\n" +
+     "a - b = " + (a - b) + "\n" +
+     "a * b = " + (a * b) + "\n" +
+     "a / b = " + (a / b) + "\n" +
+     "a % b = " + (a % b) + "\n" +
+     "(a += b): " + (a += b) + "\n" +
+     "(a -= b): " + (a -= b) + "\n" +
+     "(a *= b): " + (a *= b) + "\n" +
+     "(a /= b): " + (a /= b) + "\n" +
+     "(a %= 1): " + (a %= b) + "\n" +
+     "(a == b): " + (a == b) + "\n" +
+     "(a != b): " + (a != b) + "\n" +
+     "(a > b): " + (a > b) + "\n" +
+     "(a < b): " + (a < b) + "\n" +
+     "(!a && !c): " + (!a && !c) + "\n" +
+     "(!a || !c): " + (!a || !c) + "\n");
+*/
+
+// alert( "description of alerted_value: " + alerted_value );
+/*
+a + b
+a - b
+a * b
+b / a
+b % a
+a += 1
+a -= 1
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/ 
+
+
+/*
+//Task 2
+
+var first_name = "Brianna";
+var last_name = "Derro";
+var email = "derr0025@algonquinlive.com";
+var output = "My name is " + first_name + " " + last_name + " . You can contact me at:" + " " + email + ".";
+
+alert(output);
+console.log(("My name is " + first_name + " " + last_name + " . You can contact me at:" + " " + email + "."));
+*/
+
+
+//Task 3
+/*
+var numbers = [
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100)
+];
+
+var sum = numbers[0] + numbers[4];
+/*
+console.log(numbers[0]); // first array element
+console.log(numbers[4]); // last array element 
+*/
+/*
+console.log(numbers[0] + " + " + numbers[4] + " = " + sum);
+
+if (sum % 2 === 1) {
+    console.log("This is an odd number.");
+} else if (sum % 2 === 0) {
+    console.log("This is an even number.");    
+}
+
+/*
+// check if number is even or odd?
+5 % 2 = 1 // means a number is odd
+
+6 % 2 = 0 // means a number is even
+*/
